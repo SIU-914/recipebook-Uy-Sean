a64db8fd2d9b344c51396ea1f70719c6aad9a826
SIU-914 <ian.uy@student.ateneo.edu>
2026-02-09 19:31:14 +0800

    Add recipe views

--- ledger/views.py
+++ ledger/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+def recipe_list(request):
+    return render(request, 'recipes_list.html')
 
+def recipe_1(request):
+    return render(request, 'recipe_1.html')
+
+def recipe_2(request):
+    return render(request, 'recipe_2.html')
 # Create your views here.
