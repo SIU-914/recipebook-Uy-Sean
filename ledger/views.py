7ff80e0ec1c8e8b3b1498da74789dabe03f64fc1
SIU-914 <ian.uy@student.ateneo.edu>
2026-02-09 20:11:45 +0800

    Fix template inheritance and render pages correctly

--- ledger/views.py
+++ ledger/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
+
 def recipe_list(request):
-    return render(request, 'recipes_list.html')
+    return render(request, 'ledger/recipes_list.html')
 
 def recipe_1(request):
-    return render(request, 'recipe_1.html')
+    return render(request, 'ledger/recipe_1.html')
 
 def recipe_2(request):
-    return render(request, 'recipe_2.html')
-# Create your views here.
+    return render(request, 'ledger/recipe_2.html')
