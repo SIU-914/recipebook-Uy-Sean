988e42cf5707ec4705b7ef4f6b12b567e2deb1e9
SIU-914 <ian.uy@student.ateneo.edu>
2026-02-19 14:45:38 +0800

    Lab 2 - Database migration complete

--- ledger/urls.py
+++ ledger/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('recipes/list', views.recipe_list),
-    path('recipe/1', views.recipe_1),
-    path('recipe/2', views.recipe_2),
+    path('', views.RecipeListView.as_view(), name='recipe_list'),
+    path('recipe/<int:pk>/', views.RecipeDetailView.as_view(), name='recipe_detail'),
 ]
--- recipebook/urls.py
+++ recipebook/urls.py
@@ -18,6 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+   path('admin/', admin.site.urls),
     path('', include('ledger.urls')),
-    path('admin/', admin.site.urls),
 ]
+
+
+
+
